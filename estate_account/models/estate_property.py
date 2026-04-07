f2f8781b6d1bf07a7a9a5c7237bfbb7cf312db16
Omisen <simone.conzo@outlook.it>
2026-04-07 14:17:17 +0200

    Debug of set_status
    check of set_status logic through the inherit of estate_property in estateAccount module

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,9 +1,11 @@
 from odoo import models
 import logging
 
+_logger = logging.getLogger(__name__)
+
 class EstateProperty(models.Model):
     _inherit = ['estate.property']
     
     def set_status_to_sold(self):
-        
+        _logger.info(">>> estate_account: set_status_to_sold called")
         return super().set_status_to_sold()
