690188f7f8c81edf85ac12f54975c62fbdd64e76
Omisen <simone.conzo@outlook.it>
2026-04-07 12:50:34 +0200

    Update estate_property_views
    updated property view adding a filter domain to serach the linving area if it is equal or greather than to a specific user input both in separate fields of search

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,7 +1,9 @@
-from odoo import models, fields
+from odoo import models
+import logging
 
 class EstateProperty(models.Model):
     _inherit = ['estate.property']
     
     def set_status_to_sold(self):
+        
         return super().set_status_to_sold()
